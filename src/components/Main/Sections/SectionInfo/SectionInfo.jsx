49aecb8161b56286a957f04f6713811caa14be0b
Denys Bialy <dnsbls11@gmail.com>
2022-12-17 14:44:56 +0200

    Add CONSTANTS

--- src/components/Main/Sections/SectionInfo/SectionInfo.jsx
+++ src/components/Main/Sections/SectionInfo/SectionInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Button from '../../../Button/Button';
 import styles from './SectionInfo.module.sass';
+import CONSTANTS from '../../../../constants';
 
 const SectionInfo = () => {
     return (
         <section className={styles.imgBackground}>
             <div className={styles.positionsText}>
-                <h1>Test assignment for front-end developer</h1>
-                <p>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
+                <h1>{CONSTANTS.H1}</h1>
+                <p>{CONSTANTS.INFORMATION}</p>
                 <Button name='Sign Up' href='#signup' />
             </div>
         </section>
