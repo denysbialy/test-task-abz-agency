1ef9363d51f78136d8dd7b9515d221365e746ffd
Denys Bialy <dnsbls11@gmail.com>
2022-12-19 15:00:27 +0200

    Add component SectionSignUp

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './Main.module.sass';
 import SectionInfo from './Sections/SectionInfo/SectionInfo';
-import SectionSignUp from './Sections/SectionSignUp/SectionSignUp';
 import SectionWorkers from './Sections/SectionWorkers/SectionWorkers';
+import SectionSignUp from './Sections/SectionSignUp/SectionSignUp';
 
 const Main = () => {
     return (
