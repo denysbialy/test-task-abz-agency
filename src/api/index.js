b9c1abdd87a253a54eafad19b7c415f85b12178a
Denys Bialy <dnsbls11@gmail.com>
2022-12-20 13:52:33 +0200

    Add post request createWorker

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export 
-const workingUser = (action) =>
+export const workingUser = (action) =>
     axios.get(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=${action}`)
+    .then(res => res.data)
+export const createWorker = (values) =>
+    axios.post('https://frontend-test-assignment-api.abz.agency/api/v1/users', values)
     .then(res => res.data)
