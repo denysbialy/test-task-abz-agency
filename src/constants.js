06c5d308af2d8b11228d93b21c834a4459b72490
Denys Bialy <dnsbls11@gmail.com>
2022-12-21 22:47:45 +0200

    Create Successfully component

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 const CONSTANTS = {
     "H1" : "Test assignment for front-end developer",
     "INFORMATION" : "What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.",
-    "GET_REQUEST_H2": "Working with GET request"
+    "GET_REQUEST_H2": "Working with GET request",
+    "SUCCESSFULLY_H2": 'User successfully registered'
 };
 
 export default CONSTANTS;
