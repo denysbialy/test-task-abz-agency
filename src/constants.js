993c76f0ed9e48b34a296b7e708d875f0305f794
Denys Bialy <dnsbls11@gmail.com>
2022-12-22 16:04:28 +0200

    Add constants

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,9 @@ const CONSTANTS = {
     "H1" : "Test assignment for front-end developer",
     "INFORMATION" : "What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.",
     "GET_REQUEST_H2": "Working with GET request",
-    "SUCCESSFULLY_H2": 'User successfully registered'
+    "SUCCESSFULLY_H2": 'User successfully registered',
+    "DUBLICATE_ERROR_CREATION": 'User with this phone or email already exist',
+    "SELECT_POSITION": 'Select your position',
 };
 
 export default CONSTANTS;
