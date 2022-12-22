1ff79283abc195445a448a39c816e865e26d8b8c
Denys Bialy <dnsbls11@gmail.com>
2022-12-23 01:17:32 +0200

    Add for button clear data creation workers

--- src/components/Main/Sections/SectionInfo/SectionInfo.jsx
+++ src/components/Main/Sections/SectionInfo/SectionInfo.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../../../Button/Button';
 import styles from './SectionInfo.module.sass';
 import CONSTANTS from '../../../../constants';
+import { createWorkerClearData } from '../../../../redux/actions';
 
 const SectionInfo = () => {
+    const dispatch = useDispatch()
+    const clear = () => dispatch(createWorkerClearData());
+    const clearData = () => clear();
+
     return (
         <section className={styles.imgBackground}>
             <div className={styles.positionsText}>
                 <h1>{CONSTANTS.H1}</h1>
                 <p>{CONSTANTS.INFORMATION}</p>
-                <Button name='Sign Up' href='#signup' />
+                <Button name='Sign Up' href='#signup' handler={clearData} />
             </div>
         </section>
     );
