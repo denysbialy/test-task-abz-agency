571151c1c8ecd1b69e5ff2e4668bdf8593900bb9
Denys Bialy <dnsbls11@gmail.com>
2022-12-22 17:43:14 +0200

    The form is not completely filled out

--- src/components/Main/Sections/SectionSignUp/SectionSignUp.jsx
+++ src/components/Main/Sections/SectionSignUp/SectionSignUp.jsx
@@ -1,107 +1,61 @@
-// // import { Formik } from 'formik';
-// import React, { useEffect } from 'react';
-// import { Formik } from 'formik';
-// import { createWorkerRequest } from '../../../../redux/actions';
-// import { getToken } from '../../../../api';
-
-// const SectionSignUp = (props) => {
-// const formData = new FormData();
-// const token = () => getToken();
-
-
-//     const onChange = e => {
-//         const file = e.target.files[0];
-//         formData.append('file', file);
-//       };
-
-//   return (
-//     <Formik 
-//     initialValues = { {name: '', email: '', phone: '',}}
-//     onSubmit ={ values => { 
-      
-//       formData.append('position_id', 3);
-//       formData.append('name', values.name);
-//       formData.append('email', values.email);
-//       formData.append('phone', values.phone);
-
-//       createWorkerRequest(formData, token() );
-      
-//       }}>
-//         {
-//             ({handleSubmit, values, handleChange}) =>{
-                
-//                 return (
-//                   <form onSubmit={handleSubmit}>
-//                           <label htmlFor="name">
-//                             <input name="name" type="text" onChange={handleChange} value={values.name}/>
-//                           </label>
-//                           <label htmlFor="email">
-//                             <input name="email" type="email" onChange={handleChange} value={values.email}/>
-//                           </label>
-//                           <label htmlFor="phone">
-//                             <input name="phone" type="phone" onChange={handleChange} value={values.phone}/>
-//                           </label>
-//                           <label htmlFor="file">File Upload</label>
-//                             <input  id='file'type='file' onChange={onChange} value={values.file}/>
-                    
-//                           <button type="submit">Submit</button>
-//                         </form>
-//                       );
-//             }
-//         }
-//     </Formik>
-//   );
-// };
-
-// export default SectionSignUp;
-// import { Formik } from 'formik';
 import React, { useEffect } from 'react';
-import { Field, Form, Formik } from 'formik';
-import { createWorkerRequest, getTokenRequest } from '../../../../redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { createWorkerRequest, getPositionRequest, getTokenRequest } from '../../../../redux/actions';
+import { useDispatch, useSelector, } from 'react-redux';
+import styles from './Section.module.sass';
+import { SignupSchema } from '../../../Errors/schemaValidation';
+import Successfully from './Successfully/Successfully';
+import CONSTANTS from '../../../../constants';
+import InputForm from '../../../Inputs/basicInput/InputForm';
+import RadioInputs from '../../../Inputs/radioInputs/RadioInputs';
+import FileUploadInput from '../../../Inputs/fileInput/fileUploadInput';
 
-const SectionSignUp = (props) => {
-  const formData = new FormData();
+const initialValues = {name: '', email: '', phone: '', position_id: '', photo:''}
+
+const SectionSignUp = ({formData}) => {
   const dispatch = useDispatch();
 
-  const requestToken = () => dispatch(getTokenRequest());
-  const requestCreationWorker = (form, token) => dispatch(createWorkerRequest(form, token ))
-
-  useEffect(() => { requestToken()}, []);
-
   const {token} = useSelector((state) => state.token);
-  const data = useSelector((state) => state.creationWorker);
+  const creationData = useSelector((state) => state.creationWorker);
 
-    const onChange = e => {formData.append('photo', e.target.files[0]);};
-
-    const submitHandler = (values) =>{
-      formData.append('position_id', 3);
-      formData.append('name', values.name);
-      formData.append('email', values.email);
-      formData.append('phone', values.phone);
-      requestCreationWorker(formData, token);
-    } 
+  const requestToken = () => dispatch(getTokenRequest());
+  const requestPosition = () => dispatch(getPositionRequest());
+  const requestCreationWorker = (form, token) => dispatch(createWorkerRequest(form, token ))
+  
+  useEffect(() => { requestToken(); requestPosition()}, []);
 
   return (
     <Formik 
-    initialValues = { {name: '', email: '', phone: '',}}
-    onSubmit ={submitHandler}>
-        <Form>
-          <Field name="name" placeholder="Your Name" />
-          <Field name="email" placeholder="Your Email" />
-          <Field name="phone" placeholder="Your Phone" />
-
-          {/* {getPositions().map(e => {
-            return (
-              <label> {e.name}
-                <Field name="workerRole" type="radio" value={e.id} />
-              </label>
-            )
-          })} */}
-          <label htmlFor="file">File Upload</label>
-          <input  id='file'type='file' onChange={onChange}/>
-          <button type='submit'> Submit </button>
-        </Form>
+      initialValues = {initialValues}
+      validationSchema={SignupSchema}
+      onSubmit ={(values, {resetForm}) =>{
+        formData.append('photo', values.photo);
+        formData.append('position_id', +values.position_id);
+        formData.append('name', values.name);
+        formData.append('email', values.email);
+        formData.append('phone', values.phone);
+        resetForm(initialValues);
+        requestCreationWorker(formData, token);
+      }}
+    >
+        {({errors, touched, setFieldValue})=>(
+          <>
+          
+          {creationData.isLoading && !creationData.success.success
+          &&<div>{CONSTANTS.DUBLICATE_ERROR_CREATION}</div>}
+          
+          {creationData.isLoading && creationData.success.success ? <Successfully /> :
+          <Form className={styles.container}>
+            <InputForm name='name' errors={errors} touched={touched} />
+            <InputForm name='email' errors={errors} touched={touched} />
+            <InputForm name='phone' errors={errors} touched={touched} />
+            <RadioInputs errors={errors} touched={touched}/>
+            <FileUploadInput setFieldValue={setFieldValue} errors={errors} touched={touched}/>
+          <button type='submit'> Submit </button> 
+        </Form>}
+        
+        </>
+            )}
     </Formik>
   );
 };
