609c163a18dc720a5c9df0053c6b84c75a430b9e
Denys Bialy <dnsbls11@gmail.com>
2022-12-22 15:30:30 +0200

    Add component inputForm with validation

---
+++ src/components/Inputs/inputForm.jsx
@@ -0,0 +1,28 @@
+import { ErrorMessage, Field } from 'formik';
+import React from 'react';
+import styles from './inputForm.module.sass';
+import classNames from 'classnames';
+
+const InputForm = ({name, errors, touched}) => {
+    const input = classNames({
+        [styles.input]: true,
+        [styles.inputError]: errors[name] && touched[name]
+    } )
+    const redErrorMessage = classNames({
+        [styles.errorMessage]: errors[name] && touched[name]
+    })
+    const redLebelMessage = classNames({
+        [styles.nameInputLabel]: true,
+        [styles.lebelErrorMessage]: errors[name] && touched[name]
+    })
+    
+    return (
+            <div className={styles.container}>
+            <Field name={name} placeholder={`Your ${name}`} className={input}/>
+            <div className={redLebelMessage}>{name}</div>
+            {errors[name] && touched[name] && <ErrorMessage name={name}>{msg => <div className={redErrorMessage}>{msg}</div>}</ErrorMessage>}
+            </div>
+    );
+}
+
+export default InputForm;
